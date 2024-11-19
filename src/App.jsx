eb2690ced280339f87b041b6c42576d14231d0b3
pooya <poooyasamimi@gmail.com>
2024-11-19 18:26:25 +0330

    fix page404 src

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
 import Header from "./components/Header.jsx";
-import Sidebar from "./components/CategorySidebar.jsx";
 import { ThemeContext } from "./contexts/ThemeContext.jsx";
 import { ProductContext } from "./contexts/ProductContext.jsx";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 import { Toaster } from "react-hot-toast";
 import ChangePassword from "./pages/ChangePassword.jsx";
-import Page404 from "./pages/page404.jsx";
+import Page404 from "./pages/Page404.jsx";
 import ResetPassword from "./pages/ResetPassword.jsx";
 import LoadingAnimate from "./components/LoadingAnimate.jsx";
 import Cart from "./pages/Cart.jsx";
@@ -25,9 +24,7 @@ function App() {
     </div>
   ) : (
     <div className={`${theme === "dark" ? "dark" : ""}`}>
-      <div
-        className={`dark:text-white dark:bg-gray-700 pt-16 min-h-screen`}
-      >
+      <div className={`dark:text-white dark:bg-gray-700 pt-16 min-h-screen`}>
         <Header />
         <Toaster position="top-center" reverseOrder={false} />
         <div className="">
@@ -41,7 +38,6 @@ function App() {
             <Route path="/changepassword" element={<ChangePassword />} />
             <Route path="*" element={<Page404 />} />
           </Routes>
-
         </div>
       </div>
     </div>
